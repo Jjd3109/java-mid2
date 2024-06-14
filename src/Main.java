d8947b183211d70783ccde066c85090839f7ecb3
정종두 <jongdoo2001@naver.com>
2024-06-14 12:51:37 +0900

    study : ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//
-//        int N = sc.nextInt();
-//        int M = sc.nextInt();
-//
-//        String[][] square = new String[N][M];
-//
-//        //문제 생성
-//        for(int i = 0; i < N; i++){
-//
-//            for(int j = 0; j<M; j++){
-//                String value = sc.nextLine();
-//
-//                square[i][j] = value;
-//
-//            }
-//        }
-//
-//        //어디 부분에서 들어가는지 확인해야 한다.
-//        //1. bw의 갯수 확인
-//        //2. wb의 갯수 확인
-//
-//
-//        for(int i = 0; i < square.)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int cup = 1;
 
+        for(int i = 0; i< m; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if(x == cup){
+                cup = y;
+            }else if(y == cup){
+                cup = x;
+            }
+        }
+        System.out.println(cup);
 
     }
+
+
 }
